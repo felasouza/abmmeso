2e98aea9cee54325fc89b7ee1997cd31ef502c55
Felipe de Souza <fel.asouza@gmail.com>
2024-11-21 09:10:03 -0600

    originNode ready and tested.

---
+++ continuous/originNode.py
@@ -0,0 +1,44 @@
+
+
+class OriginNode:
+    def __init__(self, node_id, link, demands, **kwargs):
+        self.node_id = node_id
+        self.link = link
+        self.demands = demands
+        self.total_time = None
+        self.time_step = None
+        self.total_steps = None
+        self.demands_at_steps = None
+        self.entry_queue = None
+        self.outflow = None
+
+        for k,v in kwargs.items():
+            setattr(self, k, v)
+    
+
+    def start(self, time_step, total_time):
+        self.total_steps = int(total_time/time_step)
+        self.time_step = time_step
+        self.total_time = total_time
+        self._demand = None
+
+
+        self.demands_at_steps = [0 for _ in range(self.total_steps)]
+
+        ratio_steps = int(self.total_time/len(self.demands))
+        for i in range(len(self.demands)):
+            for j in range(ratio_steps):
+                self.demands_at_steps[i*ratio_steps+j] = self.demands[i]
+
+        self.entry_queue = [0 for _ in range(self.total_steps+1)]
+        self.outflow = [0 for _ in range(self.total_steps)]
+    
+    def prepare_step(self, t):
+        self._demand = self.demands_at_steps[t]+self.entry_queue[t]
+
+    def compute_flows(self, t):
+        flow = min(self._demand, self.link.get_supply())
+        self.link.set_inflow(flow)
+        self._demand = None
+        self.entry_queue[t+1] = self.entry_queue[t] + flow - self.demands_at_steps[t]
+        self.outflow[t] = flow
---
+++ tests/test_originNode.py
@@ -0,0 +1,54 @@
+import unittest
+from unittest.mock import Mock
+from continuous.originNode import OriginNode
+
+from continuous.link import Link  # Adjust the import based on the actual module structure
+
+class TestOriginNode(unittest.TestCase):
+
+    def setUp(self):
+        # Initialize a Link object and an OriginNode object before each test
+        self.link = Mock(spec=Link)
+        self.link.get_supply.return_value = 0.5
+        self.demands = [0.2, 0.6, 0.2]
+        self.origin_node = OriginNode(node_id=1, link=self.link, demands=self.demands)
+        self.origin_node.start(time_step=1, total_time=3600)
+
+    def test_initial_state(self):
+        # Test the initial state of the OriginNode object
+        self.assertIsNotNone(self.origin_node)
+        self.assertEqual(self.origin_node.node_id, 1)
+        self.assertEqual(self.origin_node.demands, self.demands)
+        self.assertEqual(self.origin_node.total_time, 3600)
+        self.assertEqual(self.origin_node.time_step, 1)
+        self.assertEqual(self.origin_node.total_steps, 3600)
+        self.assertEqual(len(self.origin_node.demands_at_steps), 3600)
+        self.assertEqual(len(self.origin_node.entry_queue), 3601)
+        self.assertEqual(len(self.origin_node.outflow), 3600)
+        self.assertEqual(self.origin_node.demands_at_steps[1199], 0.2)
+        self.assertEqual(self.origin_node.demands_at_steps[1200], 0.6)
+
+
+    def test_two_iterations(self):
+        # Test the prepare_step method
+        self.origin_node.prepare_step(0)
+        self.assertEqual(self.origin_node._demand, 0.2)
+
+        self.origin_node.compute_flows(0)
+        self.assertEqual(self.link.set_inflow.call_count, 1)
+        self.assertEqual(self.link.set_inflow.call_args[0][0], 0.2)
+        self.assertEqual(self.origin_node.entry_queue[1], 0)
+        self.assertEqual(self.origin_node.outflow[0], 0.2)
+        self.origin_node.prepare_step(1)
+        self.assertAlmostEqual(self.origin_node._demand, 0.2)
+
+
+
+
+    def tearDown(self):
+        # Clean up after each test
+        del self.origin_node
+        del self.link
+
+if __name__ == '__main__':
+    unittest.main()
