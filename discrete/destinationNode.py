6c55e2e70c272c7b9bef80520d2acbdb0929c0cd
Felipe de Souza <fel.asouza@gmail.com>
2024-11-21 20:49:11 -0600

    adds discrete destination node along with test.

---
+++ discrete/destinationNode.py
@@ -0,0 +1,27 @@
+
+
+class DestinationNode:
+    def __init__(self, node_id, link):
+        self.node_id = node_id
+        self.link = link
+        self.total_time = None
+        self.time_step = None
+        self.total_steps = None
+        self.inflow = []
+        self.arrived_vehicles = []
+
+    def start(self, time_step, total_time):
+        self.time_step = time_step
+        self.total_time = total_time
+        self.total_steps = total_time // time_step
+
+        self.inflow = [0] * self.total_steps
+
+    def prepare_step(self, t):
+        pass
+
+    def compute_flows(self, t):
+        outflow = self.link.get_demand()
+        vehicles = self.link.set_outflow(outflow)
+        self.inflow[t] = outflow
+        self.arrived_vehicles.extend(vehicles)
--- tests/test_discreteDestinationNode.py
+++ tests/test_discreteDestinationNode.py
@@ -1,25 +1,27 @@
 import unittest
 from unittest.mock import Mock
-from continuous.destinationNode import DestinationNode
-from continuous.link import Link  
+from discrete.destinationNode import DestinationNode
+from discrete.link import Link  
+
+
 
 class TestDestinationNode(unittest.TestCase):
 
     def setUp(self):
         # Initialize a Link object and a DestinationNode object before each test
         self.link = Mock(spec=Link)
-        self.link.get_demand.return_value = 0.5
+
         self.destination_node = DestinationNode(node_id=1, link=self.link)
-        self.destination_node.start(time_step=1, total_time=3600)
+        self.destination_node.start(time_step=1, total_time=120)
 
     def test_initial_state(self):
         # Test the initial state of the DestinationNode object
         self.assertIsNotNone(self.destination_node)
         self.assertEqual(self.destination_node.node_id, 1)
-        self.assertEqual(self.destination_node.total_time, 3600)
+        self.assertEqual(self.destination_node.total_time, 120)
         self.assertEqual(self.destination_node.time_step, 1)
-        self.assertEqual(self.destination_node.total_steps, 3600)
-        self.assertEqual(len(self.destination_node.inflow), 3600)
+        self.assertEqual(self.destination_node.total_steps, 120)
+        self.assertEqual(len(self.destination_node.inflow), 120)
 
     def test_prepare_step(self):
         # Test the prepare_step method
@@ -28,10 +30,13 @@ class TestDestinationNode(unittest.TestCase):
 
     def test_compute_flows(self):
         # Test the compute_flows method
+        self.link.get_demand.return_value = 1
+        self.link.set_outflow.return_value = ['vehicle1']
         self.destination_node.compute_flows(0)
         self.assertEqual(self.link.set_outflow.call_count, 1)
-        self.assertEqual(self.link.set_outflow.call_args[0][0], 0.5)
-        self.assertEqual(self.destination_node.inflow[0], 0.5)
+        self.assertEqual(self.link.set_outflow.call_args[0][0], 1)
+        self.assertEqual(self.destination_node.arrived_vehicles, ['vehicle1'])
+
 
 if __name__ == '__main__':
     unittest.main()
