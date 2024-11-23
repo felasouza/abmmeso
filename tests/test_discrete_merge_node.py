0a48e803fe0e0098e42ac39ca9e7d27784ed4745
Felipe de Souza <fel.asouza@gmail.com>
2024-11-23 11:08:38 -0600

    passing one a slightly more complex test

--- tests/test_discrete_merge_node.py
+++ tests/test_discrete_merge_node.py
@@ -25,7 +25,7 @@ class TestMergeNode(unittest.TestCase):
         self.assertEqual(merge_node.total_steps, 3600)
 
 
-    def test_compute_flows(self):
+    def test_compute_flows_simple_case(self):
         # Test the compute_flows method
 
         outbound_link = Mock(spec=Link)
@@ -52,6 +52,58 @@ class TestMergeNode(unittest.TestCase):
         inbound_links[0].set_outflow.assert_called_once_with(0)
         inbound_links[1].set_outflow.assert_called_once_with(1)
         outbound_link.set_inflow.assert_called_once_with(['vehicle1'])
+
+    def test_compute_flows_congested(self):
+        cases = [
+            {'total_steps': 4,
+             'capacity': 0.5, 'demands': {0: [0,1,0,1], 1: [1,1,1,0]}, 'expected_flows': {0: [0,1,0,1], 1: [0,0,1,0]}, 
+                'outbound_flows': [[], ['vehicle01'], ['vehicle12'], ['vehicle03']]},
+        ]
+
+
+        for case in cases:
+            outbound_link = Mock(spec=Link)
+            
+            inbound_links = []
+            for i in range(2):
+                inbound_links.append(Mock(spec=Link))
+            
+            priority_minimum_size = max(len(inbound_links), int(case['capacity'] * 2))
+            if priority_minimum_size % 2 == 1:
+                priority_minimum_size += 1
+            priority_vector = [i % 2 for i in range(priority_minimum_size)]
+
+            merge_node = MergeNode(1, outbound_link, inbound_links, priority_vector)
+            merge_node.start(time_step=1, total_time=3600)
+
+            for inb in inbound_links:
+                inb.get_cumulative_demand_term.return_value = 3
+            
+            demands = case['demands']
+            expected_flows = case['expected_flows']
+            expected_outbound_flows = case['outbound_flows']
+
+            for t in range(case['total_steps']):
+                outbound_link.get_supply.return_value = 1
+            for i, inb in enumerate(inbound_links):
+                inb.get_demand.return_value = demands[i][t]
+                if expected_flows[i][t] > 0:
+                    inb.set_outflow.return_value = [f'vehicle{i}{t}']
+                    inb.get_vehicle_from_index.return_value = f'vehicle{i}{t}'
+                print(i, t, f'vehicle{i}{t}')
+            
+            merge_node.prepare_step(t)
+            merge_node.compute_flows(t)
+            outbound_link.set_inflow.assert_called_once_with(expected_outbound_flows[t])
+            for i, inb in enumerate(inbound_links):
+                inb.set_outflow.assert_called_once_with(expected_flows[i][t])
+            
+            outbound_link.set_inflow.reset_mock()
+            for inb in inbound_links:
+                inb.set_outflow.reset_mock()
+                
+
+
 
 
 if __name__ == '__main__':
