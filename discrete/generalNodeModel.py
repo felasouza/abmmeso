cb9f67e43a4185004bcbc3e701bbec3366c40dce
Felipe de Souza <fel.asouza@gmail.com>
2024-12-02 16:55:00 -0600

    a first version of general node model

---
+++ discrete/generalNodeModel.py
@@ -0,0 +1,107 @@
+
+
+class GeneralNodeModel:
+    def __init__(self, node_id, inbound_links, outbound_links, priority_vector):
+        self.node_id = node_id
+        self.inbound_links = inbound_links
+        self.outbound_links = outbound_links
+        self.priority_vector = priority_vector
+        self.priority_index = 0
+        self.total_time = None
+        self.time_step = None
+        self.total_steps = None
+
+    def start(self, time_step, total_time):
+        self.time_step = time_step
+        self.total_time = total_time
+        self.total_steps = int(total_time / time_step)
+    
+    def prepare_step(self, t):
+        pass
+
+    def compute_flows(self, t):
+        remaining_supplies = [link.get_supply() for link in self.outbound_links]
+        remaining_demands = [link.get_demand() for link in self.inbound_links]
+        cumulative_terms = [link.get_cumulative_demand_term() for link in self.inbound_links]
+
+        flow_by_inbound_link = [0 for _ in self.inbound_links]
+        total_flow = 0
+
+        initial_index = self.priority_index
+        
+        flow_order_by_outbound_link = [[] for _ in self.outbound_links]
+        priority_base = True
+
+        while True:
+
+            if priority_base:
+                iteration_index = self.priority_index
+            idx = self.priority_vector[iteration_index]
+                
+            approach_with_priority = self.inbound_links[idx]
+            if priority_base:
+                if remaining_demands[idx] == 0 and cumulative_terms[idx]-flow_by_inbound_link[idx] > 0 :
+                    priority_base = False
+                    iteration_index = (iteration_index + 1) % len(self.priority_vector)
+                    continue
+
+                elif remaining_demands[idx] < 1:
+                    self.priority_index = (self.priority_index + 1) % len(self.priority_vector)
+                    if self.priority_index == initial_index:
+                        break
+                    continue
+            else:
+                if remaining_demands[idx] == 0:
+                    iteration_index = (iteration_index + 1) % len(self.priority_vector)
+                    if iteration_index == initial_index:
+                        break
+                    continue
+
+            inb_flow = flow_by_inbound_link[idx]
+            outbound_index = self.get_outbound_vehicle_from_vehicle(approach_with_priority.get_vehicle_from_index(inb_flow))
+            downstream_supply = remaining_supplies[outbound_index]
+            #if here now we check whether the flow is possible
+            if downstream_supply >= 1:
+                remaining_supplies[outbound_index] -= 1
+                
+                total_flow += 1
+                remaining_demands[idx] -= 1
+                vehicle = approach_with_priority.get_vehicle_from_index(inb_flow)
+                flow_order_by_outbound_link[outbound_index].append(vehicle)
+                flow_by_inbound_link[idx] += 1
+                #flow was possible to update priority index
+
+                if priority_base:
+                    self.priority_index = (self.priority_index + 1) % len(self.priority_vector)
+                    if self.priority_index == initial_index:
+                        break
+                else:
+                    iteration_index = (iteration_index + 1) % len(self.priority_vector)
+                    if iteration_index == initial_index:
+                        break
+            else:
+                iteration_index = (iteration_index + 1) % len(self.priority_vector)
+                if priority_base is False and iteration_index == initial_index:
+                    break
+                priority_base = False
+                
+
+        for idx_outb, flow in enumerate(flow_order_by_outbound_link):
+            self.outbound_links[idx_outb].set_inflow(flow)
+        
+        for idx_inb, flow in enumerate(flow_by_inbound_link):
+            self.inbound_links[idx_inb].set_outflow(flow)
+
+
+    def get_outbound_vehicle_from_vehicle(self, vehicle):
+        idx_current = -1
+
+        for idx_current, link in enumerate(vehicle.route):
+            if link in [link.link_id for link in self.inbound_links]:
+                break
+
+        outbound_id = vehicle.route[idx_current+1]
+
+        for idx_outb, link in enumerate(self.outbound_links):
+            if link.link_id == outbound_id:
+                return idx_outb
---
+++ tests/test_discrete_general_node_model.py
@@ -0,0 +1,89 @@
+import unittest
+from discrete.generalNodeModel import GeneralNodeModel
+from discrete.link import Link
+from unittest.mock import MagicMock
+
+
+class TestGeneralNodeModel(unittest.TestCase):
+
+    def setUp(self):
+       pass
+
+    def test_initialization(self):
+        # Test if the object is initialized correctly
+
+        node = self.create_node()
+
+        self.assertIsInstance(node, GeneralNodeModel)
+    
+    def test_jumping_no_demand(self):
+        case = {'demands': [1, 0, 1], 'supplies': [1, 1, 1], 'outbound_links': [0, 2],
+                'expected_outflows': [1, 0, 1], 'expected_inflows': [1, 0, 1], 'start_priority': 0,
+                'end_priority': 0}
+        self.run_case(case)
+
+    def test_stuck_in_demand(self):
+        case = {'demands': [1, 1, 1], 'supplies': [1, 0, 1], 'outbound_links': [0, 1, 1],
+                'expected_outflows': [1, 0, 0], 'expected_inflows': [1, 0, 0], 'start_priority': 0,
+                'end_priority': 1}
+        self.run_case(case)
+
+    def test_forward_mechanism_with_no_change_in_priority(self):
+        case = {'demands': [1, 1, 1], 'supplies': [1, 0, 1], 'outbound_links': [0, 1, 2],
+                'expected_outflows': [1, 0, 1], 'expected_inflows': [1, 0, 1], 'start_priority': 0,
+                'end_priority': 1}
+        self.run_case(case)
+    
+    def run_case(self, case):
+        node = self.create_node()
+        node.start(1, 60)
+
+        inbound_links = node.inbound_links
+        outbound_links = node.outbound_links
+
+        for u, demand in enumerate(case['demands']):
+            inbound_links[u].get_demand = MagicMock(return_value=demand)
+            inbound_links[u].get_cumulative_demand_term = MagicMock(return_value = demand)
+            inbound_links[u].set_outflow = MagicMock(return_value=[f'vehicle_{u}'])
+            inbound_links[u].get_vehicle_from_index = MagicMock(return_value=f'vehicle_{u}')
+
+        
+        for u, supply in enumerate(case['supplies']):
+            outbound_links[u].get_supply = MagicMock(return_value=supply)
+            outbound_links[u].set_inflow = MagicMock()
+        
+        node.get_outbound_vehicle_from_vehicle = MagicMock()
+        node.get_outbound_vehicle_from_vehicle.side_effect = case['outbound_links']
+
+        node.prepare_step(0)
+        node.priority_index = case['start_priority']
+        node.compute_flows(0)
+
+        for u, expected_flow in enumerate(case['expected_inflows']):
+            self.assertEqual(len(outbound_links[u].set_inflow.call_args[0][0]), expected_flow) 
+        
+        for u, expected_flow in enumerate(case['expected_outflows']):
+            self.assertEqual(inbound_links[u].set_outflow.call_args[0][0], expected_flow)
+        
+        self.assertEqual(node.priority_index, case['end_priority'])
+
+    def create_node(self):
+        inbound_links = []
+
+        for i in range(3):
+            link = Link(link_id = i+1, length=300, kj=0.1, w=6.0, vf=30.0)
+            inbound_links.append(link)
+        
+        outbound_links = []
+        for i in range(3):
+            link = Link(link_id = i+4, length=300, kj=0.1, w=6.0, vf=30.0)
+            outbound_links.append(link)
+        
+        priorities = [0, 1, 2]
+        node = GeneralNodeModel(1, inbound_links, outbound_links, priorities)
+        return node
+
+
+
+if __name__ == '__main__':
+    unittest.main()
