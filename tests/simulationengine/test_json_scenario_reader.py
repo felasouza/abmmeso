1619c6882c5de42b077402a66464fe8f107fb4d0
Felipe de Souza <fel.asouza@gmail.com>
2025-01-09 15:47:25 -0600

    ...

--- tests/simulationengine/test_json_scenario_reader.py
+++ tests/simulationengine/test_json_scenario_reader.py
@@ -1,5 +1,6 @@
 import unittest
 import simulationengine.jsonScenarioReader
+from continuousSingleCommodity.link import Link
 from pathlib import Path
 
 class TestJsonScenarioReader(unittest.TestCase):
@@ -7,7 +8,7 @@ class TestJsonScenarioReader(unittest.TestCase):
         pass
 
 
-    def test_sample(self):
+    def test_sample_multicommodity(self):
         reader = simulationengine.jsonScenarioReader.JSONScenarioReader(Path("tests/simulationengine/sample.json").resolve())
         reader.read()
         runner = reader.get_simulation_runner()
@@ -19,4 +20,19 @@ class TestJsonScenarioReader(unittest.TestCase):
         times = runner.get_times()
         self.assertEqual(times, [i for i in range(600)])
         total_flow = ((0.2+0.5+0.8)/3)*600
-        self.assertEqual(runner.links[0].cumulative_inflows[-1], total_flow)
+        self.assertAlmostEqual(runner.links[0].cumulative_inflows[-1], total_flow)
+
+    def test_sample_singlecommodity(self):
+        reader = simulationengine.jsonScenarioReader.JSONScenarioReader(Path("tests/simulationengine/sample_single_commodity.json").resolve())
+        reader.read()
+        runner = reader.get_simulation_runner()
+        self.assertEqual(len(runner.nodes), 4)
+        self.assertEqual(len(runner.links), 4)
+        self.assertTrue(isinstance(runner.links[0], Link))
+        self.assertEqual(runner.total_time, 600)
+        self.assertEqual(runner.time_step, 1)
+        runner.run()
+        times = runner.get_times()
+        self.assertEqual(times, [i for i in range(600)])
+        total_flow = ((0.2+0.5+0.8)/3)*600
+        self.assertAlmostEqual(runner.links[0].cumulative_inflows[-1], total_flow)
