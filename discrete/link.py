80c84193ea4d10e846ec8d36346fa922ceefa352
Felipe de Souza <fel.asouza@gmail.com>
2025-01-11 11:22:57 -0600

    linting and testing/implementing link output

--- discrete/link.py
+++ discrete/link.py
@@ -1,5 +1,6 @@
 import math
 
+
 class Link:
     def __init__(self, **kwargs):
         self.link_id = None
@@ -28,32 +29,31 @@ class Link:
         self._next_step_demand = None
         self.initial_capacity = None
 
-        for k,v in kwargs.items():
+        for k, v in kwargs.items():
             setattr(self, k, v)
 
-
     def start(self, time_step, total_time):
-        total_steps = int(total_time/time_step)
+        total_steps = int(total_time / time_step)
         self.total_steps = total_steps
         self.time_step = time_step
         self.total_time = total_time
 
-        self.cumulative_inflows = [0 for _ in range(total_steps+1)]
-        self.cumulative_outflows = [0 for _ in range(total_steps+1)]
-        self.supplies = [0 for _ in range(total_steps+1)]
-        self.cap_disc_upstream = [0 for _ in range(total_steps+1)]
-        self.cap_disc_downstream = [0 for _ in range(total_steps+1)]
+        self.cumulative_inflows = [0 for _ in range(total_steps + 1)]
+        self.cumulative_outflows = [0 for _ in range(total_steps + 1)]
+        self.supplies = [0 for _ in range(total_steps + 1)]
+        self.cap_disc_upstream = [0 for _ in range(total_steps + 1)]
+        self.cap_disc_downstream = [0 for _ in range(total_steps + 1)]
 
-        self.T1 = max(1,int(self.length/(self.vf*time_step)))
-        self.T2 = max(1, int(self.length/(self.w*time_step)))
+        self.T1 = max(1, int(self.length / (self.vf * time_step)))
+        self.T2 = max(1, int(self.length / (self.w * time_step)))
 
-        self.cap = self.kj*self.vf*self.w/(self.vf+self.w)
+        self.cap = self.kj * self.vf * self.w / (self.vf + self.w)
         if self.initial_capacity is not None:
             self.cap_disc_upstream[0] = self.initial_capacity
             self.cap_disc_downstream[0] = self.initial_capacity
         else:
-            self.cap_disc_upstream[0] = math.ceil(self.cap*self.time_step)+1
-            self.cap_disc_downstream[0] = math.ceil(self.cap*self.time_step)+1
+            self.cap_disc_upstream[0] = math.ceil(self.cap * self.time_step) + 1
+            self.cap_disc_downstream[0] = math.ceil(self.cap * self.time_step) + 1
 
     def set_inflow(self, vehicles):
         self._inflow = len(vehicles)
@@ -71,7 +71,7 @@ class Link:
 
     def get_supply(self):
         return self._supply
-    
+
     def get_cumulative_demand_term(self):
         return self._cum_demand_term
 
@@ -79,41 +79,106 @@ class Link:
         return self.vehicles[index]
 
     def update_state_variables(self, t):
-        self.cumulative_inflows[t+1] = self.cumulative_inflows[t] + self._inflow
-        self.cumulative_outflows[t+1] = self.cumulative_outflows[t] + self._outflow
+        self.cumulative_inflows[t + 1] = self.cumulative_inflows[t] + self._inflow
+        self.cumulative_outflows[t + 1] = self.cumulative_outflows[t] + self._outflow
 
-        self.cap_disc_upstream[t+1] = min(self.cap_disc_upstream[t]-self._inflow+self.cap*self.time_step, math.ceil(self.cap*self.time_step)+1)
-        self.cap_disc_downstream[t+1] = min(self.cap_disc_downstream[t]-self._outflow+self.cap*self.time_step, math.ceil(self.cap*self.time_step)+1)
+        self.cap_disc_upstream[t + 1] = min(
+            self.cap_disc_upstream[t] - self._inflow + self.cap * self.time_step,
+            math.ceil(self.cap * self.time_step) + 1,
+        )
+        self.cap_disc_downstream[t + 1] = min(
+            self.cap_disc_downstream[t] - self._outflow + self.cap * self.time_step,
+            math.ceil(self.cap * self.time_step) + 1,
+        )
 
         self._demand = None
         self._supply = None
         self._inflow = None
         self._outflow = None
-    
+
     def get_flows_in_the_past_steps(self, t, steps):
-        return self.cumulative_outflows[t]-self.cumulative_outflows[t-steps]
+        return self.cumulative_outflows[t] - self.cumulative_outflows[t - steps]
 
     def compute_demand_and_supplies(self, t):
-        if t < self.T1-1:
+        if t < self.T1 - 1:
             self._demand = 0
             self._cum_demand_term = 0
             self._next_step_demand = 0
         else:
-            self._demand = min(math.floor(self.cumulative_inflows[t-self.T1+1]-self.cumulative_outflows[t]), 
-                               math.floor(self.cap_disc_downstream[t]))
-            self._cum_demand_term = self.cumulative_inflows[t-self.T1+1]-self.cumulative_outflows[t]
+            self._demand = min(
+                math.floor(
+                    self.cumulative_inflows[t - self.T1 + 1]
+                    - self.cumulative_outflows[t]
+                ),
+                math.floor(self.cap_disc_downstream[t]),
+            )
+            self._cum_demand_term = (
+                self.cumulative_inflows[t - self.T1 + 1] - self.cumulative_outflows[t]
+            )
 
-            queue_addition = self.cumulative_inflows[t-self.T1+2]-self.cumulative_inflows[t-self.T1+1]
+            queue_addition = (
+                self.cumulative_inflows[t - self.T1 + 2]
+                - self.cumulative_inflows[t - self.T1 + 1]
+            )
 
             if queue_addition > 0 or self._cum_demand_term > 0:
                 self._next_step_demand = 1
             else:
                 self._next_step_demand = 0
 
-        if t < self.T2-1:
+        if t < self.T2 - 1:
             self._supply = int(self.cap_disc_upstream[t])
         else:
-            self._supply = min(math.floor(self.kj*self.length+ self.cumulative_outflows[t-self.T2+1]-self.cumulative_inflows[t]), 
-                               math.floor(self.cap_disc_upstream[t]))
+            self._supply = min(
+                math.floor(
+                    self.kj * self.length
+                    + self.cumulative_outflows[t - self.T2 + 1]
+                    - self.cumulative_inflows[t]
+                ),
+                math.floor(self.cap_disc_upstream[t]),
+            )
             self._supply = int(self._supply)
-            self._cum_supply_term = math.floor(self.kj*self.length+self.cumulative_outflows[t-self.T2+1]-self.cumulative_inflows[t])
+            self._cum_supply_term = math.floor(
+                self.kj * self.length
+                + self.cumulative_outflows[t - self.T2 + 1]
+                - self.cumulative_inflows[t]
+            )
+
+    def get_output_records(self, time_step):
+        total_steps = int(self.total_time / time_step)
+        records = []
+
+        for t in range(total_steps + 1):
+            start_step = int((t * time_step) / self.time_step)
+            next_st = min(int(((t + 1) * time_step) / self.time_step), len(self.cumulative_inflows)-1)
+
+            if t == 0:
+                cumulative_inflow = 0
+                cumulative_outflow = 0
+            else:
+                cumulative_inflow = self.cumulative_inflows[start_step]
+                cumulative_outflow = self.cumulative_outflows[start_step]
+
+            if t < total_steps:
+                inflow = (
+                    self.cumulative_inflows[next_st]
+                    - self.cumulative_inflows[start_step]
+                ) / time_step
+
+                outflow = (
+                    self.cumulative_outflows[next_st]
+                    - self.cumulative_outflows[start_step]
+                ) / time_step
+            else:
+                inflow = None
+                outflow = None
+
+            record = {
+                "time": t * time_step,
+                "inflow": inflow,
+                "outflow": outflow,
+                "cumulative_inflow": cumulative_inflow,
+                "cumulative_outflow": cumulative_outflow,
+            }
+            records.append(record)
+        return records
--- tests/discrete/test_discrete_link.py
+++ tests/discrete/test_discrete_link.py
@@ -2,8 +2,20 @@ import unittest
 from discrete.link import Link
 from demand.trip import Trip
 
+
 def vehicle_trip(trip_id, step):
-    return Trip(trip_id=trip_id, origin=1, destination=2, start=step, route=[1,], end=step+1, travel_times=[])
+    return Trip(
+        trip_id=trip_id,
+        origin=1,
+        destination=2,
+        start=step,
+        route=[
+            1,
+        ],
+        end=step + 1,
+        travel_times=[],
+    )
+
 
 class TestLink(unittest.TestCase):
 
@@ -30,7 +42,7 @@ class TestLink(unittest.TestCase):
         self.assertEqual(supply, self.link.cap_disc_upstream[0])
 
     def test_demand_increase(self):
-        self.link.start(time_step=1, total_time=30)
+        self.link.start(time_step=1, total_time=10)
         total_vehicles = 0
         for t in range(self.link.T1):
             self.link.compute_demand_and_supplies(t)
@@ -41,19 +53,43 @@ class TestLink(unittest.TestCase):
                 self.link.set_inflow([])
             self.link.set_outflow(0)
             self.link.update_state_variables(t)
-            self.assertEqual(self.link.cumulative_inflows[t+1]-self.link.cumulative_outflows[t+1], len(self.link.vehicles))
-        
+            self.assertEqual(
+                self.link.cumulative_inflows[t + 1]
+                - self.link.cumulative_outflows[t + 1],
+                len(self.link.vehicles),
+            )
+
         self.link.compute_demand_and_supplies(self.link.T1)
         self.assertEqual(self.link.get_demand(), 1.0)
-        self.assertEqual(self.link.cumulative_inflows[self.link.T1], self.link.T1/2+1)
+        self.assertEqual(
+            self.link.cumulative_inflows[self.link.T1], self.link.T1 / 2 + 1
+        )
 
         len_before = len(self.link.vehicles)
 
         vehicle = self.link.set_outflow(1)
         self.assertEqual(vehicle[0].trip_id, 0)
-        self.assertEqual(len(self.link.vehicles), len_before-1)
-        
-    
+        self.assertEqual(len(self.link.vehicles), len_before - 1)
+
+        output_records = self.link.get_output_records(self.link.time_step)
+        self.assertEqual(len(output_records), 11)
+        self.assertEqual(
+            output_records[0]["inflow"],
+            self.link.cumulative_inflows[1] - self.link.cumulative_inflows[0],
+        )
+        self.assertEqual(
+            output_records[1]["outflow"],
+            self.link.cumulative_outflows[1] - self.link.cumulative_outflows[0],
+        )
+
+        output_records_half = self.link.get_output_records(self.link.time_step * 2)
+
+        self.assertEqual(len(output_records_half), 6)
+        output_one = self.link.get_output_records(10)
+        self.assertEqual(len(output_one), 2)
+        one_inflow = output_records[9]["cumulative_inflow"]-output_records[0]["cumulative_inflow"]
+        self.assertEqual(output_one[0]["inflow"], one_inflow/10.0)
+
     def test_supply_decrease(self):
         self.link.start(time_step=1, total_time=180)
         inflow_plan = 0.5
@@ -62,7 +98,7 @@ class TestLink(unittest.TestCase):
         total_vehicles = 0
         for t in range(required_steps):
             self.link.compute_demand_and_supplies(t)
-            if t%2 == 0:
+            if t % 2 == 0:
                 self.link.set_inflow([vehicle_trip(t, t)])
                 total_vehicles += 1
             else:
@@ -84,24 +120,20 @@ class TestLink(unittest.TestCase):
             link.set_inflow([vehicle_trip(i, i)])
             link.set_outflow(0)
             link.update_state_variables(i)
-        
-        self.assertEqual(link.get_next_step_demand(),1)
-        for i in range(5,10):
+
+        self.assertEqual(link.get_next_step_demand(), 1)
+        for i in range(5, 10):
             link.compute_demand_and_supplies(i)
             link.set_inflow([])
             link.set_outflow(1)
             link.update_state_variables(i)
-        
 
         self.assertEqual(link.get_flows_in_the_past_steps(10, 5), 5)
         self.assertEqual(link.get_flows_in_the_past_steps(10, 1), 1)
 
-
-
-
-
     def tearDown(self):
         del self.link
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
